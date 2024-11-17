6c54c9923acb3a6462fa93a8585e4e93437627a6
dmondal06 <deepa.mondal321@gmail.com>
2024-11-17 13:58:51 -0500

    updated app container to integrate room database

---
+++ app/src/main/java/com/example/inventory/data/ItemDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.inventory.data
+
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import android.content.Context
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class ItemDatabase : RoomDatabase() {
+
+    abstract fun itemDao(): ItemDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ItemDatabase? = null
+
+        fun getDatabase(context: Context): ItemDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ItemDatabase::class.java,
+                    "item_database"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
